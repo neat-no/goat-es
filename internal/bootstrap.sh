8fcfd587349e3befb920af5104d35b8f9827ec6a
Sam Jansen <sam@avos.io>
2023-12-13 09:51:54 +0000

    Sort out dprint. Tidy.

--- internal/bootstrap.sh
+++ internal/bootstrap.sh
@@ -4,17 +4,24 @@ set -e
 rootdir="$(pwd)"
 bindir="${rootdir}/internal/.bin"
 checkdir="${rootdir}/internal/sha"
+dprint_dir="${rootdir}/internal/dprint"
 
 check_bin() {
 	local name="$1"
 	local check="${checkdir}/${name}.sha1sum"
-	[ -f "${check}" ] &&
-	sha1sum --status --check "${check}"
-	# sha1sum "${bindir}/${name}"
+	[ -f "${check}" ] && sha1sum --status --check "${check}"
+}
+
+check_dprint() {
+	local name="$1"
+	local check="${checkdir}/${name}.sha1sum"
+
 }
 
 install_buf() {
 	mkdir -p "${bindir}"
+
+	check_bin "buf" && return
 
 	BIN="${bindir}" && \
 	VERSION="1.28.1" && \
@@ -25,4 +32,15 @@ install_buf() {
 	chmod +x "${BIN}/buf"
 }
 
-check_bin "buf" || install_buf
+install_dprint_plugin() {
+	local name="$1"
+	local plugin="${dprint_dir}/${name}"
+
+	check_bin "${name}" && return
+	mkdir -p "${dprint_dir}"
+	curl -fsSL "https://plugins.dprint.dev/${name}" --output "${plugin}"
+}
+
+install_buf
+install_dprint_plugin "typescript-0.88.1.wasm"
+install_dprint_plugin "json-0.17.4.wasm"
