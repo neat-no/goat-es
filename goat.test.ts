7d42e59231b3cefdc822ab3fc01950579d3a2166
Dominik Strak <dominik@avos.io>
2023-12-20 11:17:41 +0000

    change so I can comment

--- goat.test.ts
+++ goat.test.ts
@@ -4,7 +4,7 @@ import { TestService } from "gen/testproto/test_connect";
 import { Msg } from "gen/testproto/test_pb";
 import { Body, RequestHeader, ResponseStatus, Rpc } from "gen/goatorepo/rpc_pb";
 import { GoatTransport } from "goat";
-import { vi } from "vitest"; 
+import { vi } from "vitest";
 
 class AwaitableQueue<T> {
     private q = Array<T>();
@@ -22,7 +22,7 @@ class AwaitableQueue<T> {
                 this.notify = r;
             });
         }
-        return this.q.shift()!!;
+        return this.q.shift()!!; 
     }
 }
 
